8460871e2a192484c66e2e36087634c40a52470f
Rovic Castro <193394253+roviccastro@users.noreply.github.com>
2025-01-14 22:11:06 +0800

    Fix: Solve test 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+  let finalString = ``;
+  let lengthString = str.length;
+
+  for (let a = lengthString - 1; a >= 0; a--){
+    finalString += `${str.at(a)}`;
+  };
+
+  return finalString;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+// npm test 03_reverseString/reverseString.spec.js
