0cd278a4da4631aae1297cfc428734d4849a14df
Rovic Castro <193394253+roviccastro@users.noreply.github.com>
2025-01-14 21:59:01 +0800

    Fix: Solve test 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
 
+  let finalString = ``;
+
+  if (num < 0) finalString = `ERROR`;
+
+  while (num > 0){
+
+    finalString += str;
+    num--;
+  }
+
+  return finalString;
 };
 
 // Do not edit below this line
