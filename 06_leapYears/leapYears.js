bab04a7083626dad833e9dc13be87fa5cad68e6f
Rovic Castro <193394253+roviccastro@users.noreply.github.com>
2025-01-15 22:38:57 +0800

    Fix: Solve test 6
    - Lol this was fun, at first I played with
    operators and wanted to nested ifs, but
    I realized if I nested ifs, it would
    take long and is just not viable. I then
    came to realization that changing operator
    precedence, will have the result as
    nested ifs.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  let isLeapYear = false;
 
+  if (year % 4 === 0 && 
+    (year % 400 === 0 || !(year % 100 == 0))){
+    isLeapYear = true
+  }
+
+  return isLeapYear;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+// npm test 06_leapYears/leapYears.spec.js
