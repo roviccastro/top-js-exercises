d71c23675c54498cca2a6efbc51c02d2a97fd261
Rovic Castro <193394253+roviccastro@users.noreply.github.com>
2025-01-15 22:01:33 +0800

    Fix: Solve test 5
    - I went the hard and long way, I feel
    like there is an optimal and shorter way
    of solving the test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+  let numSum = 0;
 
+  if (typeof numOne == 'number' && 
+    typeof numTwo == 'number' && 
+    Number.isInteger(numOne) &&
+    Number.isInteger(numOne)){
+
+    if (numOne < 0 || numTwo < 0){
+      numSum = 'ERROR';
+    } else if (numOne > numTwo){
+      for (let a = numOne; a >= numTwo; --a){
+        numSum += a;
+      }
+    } else if (numOne < numTwo){
+      for (let a = numOne; a <= numTwo; ++a){
+        numSum += a;
+      }
+    } else {
+      numSum = 'ERROR';
+    }
+
+  } else {
+    numSum = 'ERROR';
+  }
+
+  return numSum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+// npm test 05_sumAll/sumAll.spec.js
