5bc144974e70c693d36710f5ef8acb5b0a63803b
Rovic Castro <193394253+roviccastro@users.noreply.github.com>
2025-01-15 22:58:44 +0800

    Fix: Solve test 5
    - (Developer's Note: All the extra body on the
    previous commits are just for fun and are
    not to be taken seriously (by job
    recruiters.))
    - Lol having used + instead of Number() felt
    great. At first I was using ++ because
    I forgot ++ is actually a prefix operator.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+
+  return (+((degree - 32) * (5/9)).toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+
+  return (+((degree * 1.8) + 32).toFixed(1));
+
 };
 
 // Do not edit below this line
@@ -9,3 +15,5 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+// npm test 07_tempConversion/tempConversion.spec.js
