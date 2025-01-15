c24cfcd0a010d16b72637f638168809e9a0f2d49
Rovic Castro <193394253+roviccastro@users.noreply.github.com>
2025-01-15 20:32:50 +0800

    Fix: Solve test 4
    - It took me a total of almost a day to solve
    this test. Almos gave up bruh. I learnt
    that I can manipulate methods that returns
    a boolean value (like .includes) for methods
    that requires a truthy to perform (like
    .filter), this was challenging. I saw the
    method at stackOverflow and I hope this
    is okay.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(original, ...modifier) {
+
+  let result = function(){
+    return (original.filter(o => !modifier.includes(o)));
+  }
+
+  return (result());
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// npm test 04_removeFromArray/removeFromArray.spec.js
