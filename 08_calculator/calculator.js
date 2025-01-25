4df2f0ace1a0bcb9a831baf02a3bc4b04a1f50f3
Rovic Castro <193394253+roviccastro@users.noreply.github.com>
2025-01-25 23:20:13 +0800

    Fix: Solve 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,63 @@
-const add = function() {
-	
+const add = function(a, b) {
+  let output = 0;
+
+  output = a + b;
+
+  return output;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let output = 0;
+
+  output = a - b;
+
+  return output; 
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	let output = 0;
+
+  output = a.reduce((total, num) => {
+    return total + +num;
+  }, 0)
+
+  return output;
 };
 
-const multiply = function() {
+const multiply = function(a) {
+  let output = 0;
 
+  output = a.reduce((total, num) => {
+    return total * num;
+  })
+
+  return output;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let output = 0;
+
+  output = a ** b;
+
+  return output;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let output = a;
+  
+  if (a === 0 || a === 1){
+    return output = 1;
+  }
+
+  while (a > 1){
+    a--;
+    output *= a;
+  }
+
+  return output;
 };
+
+// npm test 08_calculator/calculator.spec.js
 
 // Do not edit below this line
 module.exports = {
