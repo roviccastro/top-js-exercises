014e259e24ba50e0ac78f9805b2d686364f642b9
Rovic Castro <193394253+roviccastro@users.noreply.github.com>
2025-01-27 21:00:07 +0800

    Fix: Solve 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+  return arr.map((a) => a.title);
 };
 
+// npm test 11_getTheTitles/getTheTitles.spec.js
 // Do not edit below this line
 module.exports = getTheTitles;
