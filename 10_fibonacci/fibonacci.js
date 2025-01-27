53d7ae25e26dd53b79b093ba5d40209d8c9049c1
Rovic Castro <193394253+roviccastro@users.noreply.github.com>
2025-01-27 20:48:04 +0800

    Fix: Solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+
+  th = parseInt(a);
+
+  let sequence = [0, 1];
+
+  if (th === 0){
+    return 0;
+  } else if (th < 0){
+    return 'OOPS'
+  }
+  
+  while (!sequence[th]){
+    sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 1 - 1])
+    console.log(sequence);
+  }
+  
+  return sequence[sequence.length - 1];
 
 };
+
+// npm test 10_fibonacci/fibonacci.spec.js
 
 // Do not edit below this line
 module.exports = fibonacci;
