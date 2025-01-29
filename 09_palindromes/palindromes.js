bcb6b993f29fef013ace1994e269d34531e7e467
Rovic Castro <193394253+roviccastro@users.noreply.github.com>
2025-01-29 22:50:42 +0800

    Style: Remove comments

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,46 +1,5 @@
 const palindromes = function (a) {
-  // let output = '';
-
-  // let reversedWord = a.split('').reverse().join('');
-
-  // for (x of reversedWord){
-  //   output += x;
-  // }
-
-
-  // let checker = () => {
-  //   if (output.toLowerCase === a.toLowerCase){
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // }
-
-  // console.log(output);
-  // return checker();
-
-  // let originalStr = a.split('').join('').toLowerCase().split('')
-  // let palindromeStr = a.split('').reverse().join().toLowerCase().split(',');
-
-  // function isPalindrome(org, str){
-    
-  //   for (letter in originalStr){
-  //     for (char in palindromeStr){
-  //       if (originalStr[letter] === palindromeStr[char]){
-  //         return true
-  //       } else {
-  //         return false
-  //       }
-  //     }
-  //   }
-
-  // }
-
-  // return isPalindrome(originalStr, palindromeStr);
-
-  // let originalStr = a;
-  // let reversedStr = a.split('').join('')
-
+ 
   let original = () => {
     arr = a.split('');
     
